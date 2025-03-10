cc978fa8be741280435b0f4c23985ece2c5a67d5
noor <noorhafowbare@gmail.com>
2025-03-10 09:47:10 +0300

    initial commit

---
+++ primitive.js
@@ -0,0 +1,26 @@
+// primitive types
+// 1. Number
+let num = 10;
+console.log(num);
+console.log(typeof num);
+
+// basic math
+// console.log(2 + 2);
+// console.log(2 - 2);
+// console.log(2 * 2);
+// console.log(4 / 2);
+// console.log(2 % 2);
+
+let counter = 0;
+// increment counter
+counter++;
+counter++;
+counter++;
+counter++;
+counter++;
+console.log(counter);
+
+// decrement counter
+let decrement = 11;
+decrement--;
+console.log(decrement);
